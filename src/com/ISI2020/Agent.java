de4861f95537e17a3dc1d528233c26ebd2308ce1
fpieschaconr <fpieschaconr@unal.edu.co>
2020-09-14 10:08:42 -0500

    ya funciona

--- src/com/ISI2020/Agent.java
+++ src/com/ISI2020/Agent.java
@@ -1,6 +1,7 @@
 package com.ISI2020;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Agent {
@@ -17,6 +18,8 @@ public class Agent {
     private boolean group;
     private ArrayList<String> permutations;
     private Random random;
+    private boolean midPhase;
+    private boolean used;
 
     public Agent(){
         this.guess="0789";
@@ -31,6 +34,8 @@ public class Agent {
         this.permutations = new ArrayList<>();
         this.random = new Random();
         this.group=false;
+        this.midPhase=false;
+        this.used=false;
     }
 
     public String compute(String action){
@@ -50,9 +55,11 @@ public class Agent {
             this.known = new int[][] {{-1,-1}, {-1,-1}, {-1,-1}, {-1,-1}};
             this.equalCounter=0;
             this.acc=0;
-            this.group=false;
             this.permutations = new ArrayList<>();
             this.random = new Random();
+            this.group=false;
+            this.midPhase=false;
+            this.used=false;
             return finalGuess;
         }
         return "WAITING VALID STATE";
@@ -64,7 +71,7 @@ public class Agent {
             this.picas=picas;
             this.fijas=fijas;
 
-            if (!this.stage){
+            if (!this.stage&&!this.midPhase){
 
                 if (this.fijas + this.picas > 3){ //se consiguen los 4 digitos, 789 son parte
 
@@ -78,8 +85,15 @@ public class Agent {
 
                 }else if(!this.guess.equals("0789")){ //para turno 2 en adelante
                     //para conseguir los 4 digitos
-                    if (this.fijas<this.prevFijas){ //si bajan las fijas quiere decir que la anterior era fija
-                        this.known[0][0]=Character.getNumericValue(this.guess.charAt(0))-1;
+                    if(this.fijas<this.prevFijas&&this.guess.equals("1789")){
+                        this.known[0][0]=0;
+                        this.known[0][1]=1;
+                        this.acc++;
+                        if(this.acc>=4){ //si entra aca, ya se tienen los digitos
+                            this.stage=true;
+                        }
+                    }else if (this.fijas>this.prevFijas){ //si suben las fijas quiere decir que es fija
+                        this.known[0][0]=Character.getNumericValue(this.guess.charAt(0));
                         this.known[0][1]=1;
                         this.acc++;
                         if(this.acc>=4){ //si entra aca, ya se tienen los digitos
@@ -124,44 +138,170 @@ public class Agent {
 
                     }
                     this.guess=this.guess.replace(this.guess.charAt(0), (char) (this.guess.charAt(0)+1));
-                    boolean p = true;
-                    int q = random.nextInt(6);
-                    if (this.guess.charAt(0)=='7'||this.guess.charAt(0)=='8'||this.guess.charAt(0)=='9'){
-                        while(p){
-                            if (this.guess.contains(String.valueOf(q))){
-                                q = random.nextInt(6);
-                                p=true;
-                            }else{
-                                p=false;
-                            }
-                            for (int i = 0; i<4; i++){
-                                if (this.known[0][0]==q){
-                                    q = random.nextInt(6);
-                                    p=true;
-                                    break;
-                                }else{
-                                    p=false;
-                                }
-                            }
-                        }
-                    }
-                    if (this.guess.charAt(0)=='7'){ //si entra en estos if, en 789 hay digito(s) del numero
-
-                        this.guess = this.guess.substring(0,1)+String.valueOf(q)+this.guess.substring(2);
-
-                    }else if (this.guess.charAt(0)=='8'){
-
-                        this.guess = this.guess.substring(0,2)+String.valueOf(q)+this.guess.substring(3);
-
-                    }else if (this.guess.charAt(0)=='9'){
-
-                        this.guess = this.guess.substring(0,3)+String.valueOf(q);
-
+                    if (this.guess.equals("7789")){
+                        this.midPhase=true;
                     }
 
                 }else { //para turno 1
 
                     this.guess=this.guess.replace(this.guess.charAt(0), (char) (this.guess.charAt(0)+1));
+                    if (this.picas>0){this.equalCounter=1;}
+
+                }
+
+            }
+            if (!this.stage&&this.midPhase){//si entra en estos if, en 789 hay digito(s) del numero
+
+                boolean p = true;
+                int q = random.nextInt(6);
+                while(p){
+                    if (this.guess.contains(String.valueOf(q))){
+                        q = random.nextInt(6);
+                        p =true;
+                    }else{
+                        p=false;
+                    }
+                    for (int i = 0; i<4; i++){
+                        if (this.known[i][0]==q){
+                            q = random.nextInt(6);
+                            p=true;
+                            break;
+                        }else{
+                            p=false;
+                        }
+                    }
+                }
+
+                if(this.used){
+                    System.out.println(this.acc);
+                    if (this.fijas+this.picas<this.prevFijas+this.prevPicas){
+                        if (this.guess.contains("8")){
+                            if (this.fijas+this.picas<this.prevFijas+this.prevPicas-1){
+                                int i = 1;
+                                while(i<3&&this.known[i][0]!=-1){
+                                    i++;
+                                }
+                                if (this.known[0][0]==-1){i=0;}
+                                this.known[i][0]=7;
+                                this.known[i][1]=0;
+                                this.acc++;
+                                System.out.println(Arrays.deepToString(this.known));
+                                if(this.acc>=4){ //si entra aca, ya se tienen los digitos
+                                    this.stage=true;
+                                    this.midPhase=false;
+                                }
+                            }
+                            if (this.picas+this.fijas==0){
+                                int i = 1;
+                                while(i<3&&this.known[i][0]!=-1){
+                                    i++;
+                                }
+                                if (this.known[0][0]==-1){i=0;}
+                                this.known[i][0]=6;
+                                this.known[i][1]=0;
+                                this.acc++;
+                                System.out.println(Arrays.deepToString(this.known));
+                                if(this.acc>=4){ //si entra aca, ya se tienen los digitos
+                                    this.stage=true;
+                                    this.midPhase=false;
+                                }
+                            }
+                        }else if (this.guess.contains("9")){
+                            int i = 1;
+                            while(i<3&&this.known[i][0]!=-1){
+                                i++;
+                            }
+                            if (this.known[0][0]==-1){i=0;}
+                            this.known[i][0]=8;
+                            this.known[i][1]=0;
+                            this.acc++;
+                            System.out.println(Arrays.deepToString(this.known));
+                            if(this.acc>=4){ //si entra aca, ya se tienen los digitos
+                                this.stage=true;
+                                this.midPhase=false;
+                            }
+                            if (this.picas+this.fijas==0){
+                                i = 1;
+                                while(i<3&&this.known[i][0]!=-1){
+                                    i++;
+                                }
+                                if (this.known[0][0]==-1){i=0;}
+                                this.known[i][0]=6;
+                                this.known[i][1]=0;
+                                this.acc++;
+                                System.out.println(Arrays.deepToString(this.known));
+                                if(this.acc>=4){ //si entra aca, ya se tienen los digitos
+                                    this.stage=true;
+                                    this.midPhase=false;
+                                }
+                            }
+                        }else{
+                            int i = 1;
+                            while(i<3&&this.known[i][0]!=-1){
+                                i++;
+                            }
+                            if (this.known[0][0]==-1){i=0;}
+                            this.known[i][0]=9;
+                            this.known[i][1]=0;
+                            this.acc++;
+                            System.out.println(Arrays.deepToString(this.known));
+                            if(this.acc>=4){ //si entra aca, ya se tienen los digitos
+                                this.stage=true;
+                                this.midPhase=false;
+                            }
+                            if (this.picas+this.fijas==0){
+                                i = 1;
+                                while(i<3&&this.known[i][0]!=-1){
+                                    i++;
+                                }
+                                if (this.known[0][0]==-1){i=0;}
+                                this.known[i][0]=6;
+                                this.known[i][1]=0;
+                                this.acc++;
+                                System.out.println(Arrays.deepToString(this.known));
+                                if(this.acc>=4){ //si entra aca, ya se tienen los digitos
+                                    this.stage=true;
+                                    this.midPhase=false;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (this.guess.contains("7")){
+
+                    this.guess = String.valueOf(q)+this.guess.substring(1);
+                     p = true;
+                     q = random.nextInt(6);
+                    while(p){
+                        if (this.guess.contains(String.valueOf(q))){
+                            q = random.nextInt(6);
+                            p =true;
+                        }else{
+                            p=false;
+                        }
+                        for (int i = 0; i<4; i++){
+                            if (this.known[i][0]==q){
+                                q = random.nextInt(6);
+                                p=true;
+                                break;
+                            }else{
+                                p=false;
+                            }
+                        }
+                    }
+                    this.guess = this.guess.substring(0,1)+String.valueOf(q)+this.guess.substring(2);
+                    this.used=true;
+
+                }else if (this.guess.contains("8")){
+
+                    this.guess = this.guess.substring(0,2)+String.valueOf(q)+this.guess.substring(3);
+                    this.used=true;
+
+                }else if (this.guess.contains("9")){
+
+                    this.guess = this.guess.substring(0,3)+String.valueOf(q);
+                    this.used=true;
 
                 }
 
